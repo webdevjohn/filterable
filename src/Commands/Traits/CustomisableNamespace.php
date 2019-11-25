3a9d2a57c16565b62408a24c108510cd6098268e
webdevjohn <pye.john@gmail.com>
2019-11-25 22:33:54 +0000

    Str::studly

--- src/Commands/Traits/CustomisableNamespace.php
+++ src/Commands/Traits/CustomisableNamespace.php
@@ -2,6 +2,8 @@
 
 namespace Webdevjohn\Filterable\Commands\Traits;
 
+use Illuminate\Support\Str;
+
 trait CustomisableNamespace {
 
     /**
@@ -29,7 +31,7 @@ trait CustomisableNamespace {
     protected function createCustomNamespace(string $dummyNamespace)
     {
         foreach ($this->getNamespaceElements($dummyNamespace) as $dummyNamespaceElement) {
-            $processed[] = studly_case($dummyNamespaceElement);
+            $processed[] = Str::studly($dummyNamespaceElement);
         }
            
         return implode($processed, '\\');
